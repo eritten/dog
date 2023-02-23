a556bbbac3b56f27a0b178f8822c7b85ef9837f0
eritten <59349542+eritten@users.noreply.github.com>
2023-02-22 21:11:12 -0800

    adding urls route

--- dog/urls.py
+++ dog/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 from django.urls import path
-from home.views import home, detail
+from home.views import home, detail, privacy, terms
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
+path('privacy/', privacy, name='privacy'),
+path('terms/', terms, name='terms'),
 path('', home, name='home'),
 path('detail/<int:pk>/<str:name>/', detail, name='detail'),
     path('admin/', admin.site.urls),
