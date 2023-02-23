2f4ad434937807cbf468c092975ab9035cb4efac
eritten <59349542+eritten@users.noreply.github.com>
2023-02-22 17:08:09 -0800

    add changes

--- dog/urls.py
+++ dog/urls.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 from home.views import home, detail
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
 path('', home, name='home'),
 path('detail/<int:pk>/<str:name>/', detail, name='detail'),
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
